1433a34a1f2a3f92d98cd825f91244d780e65429
NoahMelle <noahmkamphuisen@gmail.com>
2025-01-07 17:49:28 +0100

    Added basic player movement

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { OrbitControls, KeyboardControls, useGLTF } from "@react-three/drei";
+import { Physics, RigidBody } from "@react-three/rapier";
+import { Canvas } from "@react-three/fiber";
+import Ecctrl from "ecctrl";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const keyboardMap = [
+        { name: "forward", keys: ["ArrowUp", "KeyW"] },
+        { name: "backward", keys: ["ArrowDown", "KeyS"] },
+        { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+        { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+        { name: "jump", keys: ["Space"] },
+        { name: "run", keys: ["Shift"] },
+        // Optional animation key map
+        { name: "action1", keys: ["1"] },
+        { name: "action2", keys: ["2"] },
+        { name: "action3", keys: ["3"] },
+        { name: "action4", keys: ["KeyF"] },
+    ];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Canvas>
+            <ambientLight />
+            <Physics debug={true}>
+                <Rink />
+                <KeyboardControls map={keyboardMap}>
+                    <Ecctrl disableFollowCam={false} position={[0, 5, -10]}>
+                        <Player />
+                    </Ecctrl>
+                </KeyboardControls>
+                <Puck />
+            </Physics>
+            <OrbitControls />
+        </Canvas>
+    );
 }
 
-export default App
+const Plane = () => {
+    return (
+        <RigidBody type="fixed">
+            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+                <planeGeometry args={[30, 30]} />
+                <meshStandardMaterial color="hotpink" />
+            </mesh>
+        </RigidBody>
+    );
+};
+
+const Puck = () => {
+    return (
+        <RigidBody type="dynamic" colliders="trimesh" mass={0.1} >
+            <mesh position={[0, 4, 0]}>
+                <sphereGeometry args={[0.3]} />
+                <meshStandardMaterial color="red" />
+            </mesh>
+        </RigidBody>
+    );
+}
+
+const Player = () => {
+    return (
+        // <RigidBody>
+        <mesh>
+            <capsuleGeometry args={[0.3, 0.7, 3]} />
+            <meshStandardMaterial color="blue" />
+        </mesh>
+        // </RigidBody>
+    );
+};
+
+const Rink = () => {
+    return (
+        <ModelWithCollision url="/rink.glb" />
+    );
+}
+
+function ModelWithCollision({ url }: { url: string }) {
+    const { scene } = useGLTF(url);
+
+    return (
+        <RigidBody type="fixed" colliders="trimesh">
+            <primitive object={scene} />
+        </RigidBody>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import * as THREE from 'three'
+import createStore from 'zustand'
+import { Canvas } from "@react-three/fiber";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
+
+export const useStore = createStore((set) => ({
+    scene: new THREE.Scene(),
+}));
