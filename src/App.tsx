e491d2f703684e45f5fc7a2e2b5f83508c755381
PepijnBullens <pepijnpersoonlijk@gmail.com>
2025-01-17 09:29:23 +0100

    stash

--- src/App.tsx
+++ src/App.tsx
@@ -4,39 +4,78 @@ import LoadingWrapper from "./components/LoadingWrapper";
 import Snowfall from "react-snowfall";
 import Scene from "./Scene";
 
-export default function App() {
-    const [score, setScore] = useState({ p1: 0, p2: 0 });
-    const [lastPuckReset, setLastPuckReset] = useState(Date.now());
-
-    useEffect(() => {
-        console.log(score);
-        setLastPuckReset(Date.now());
-    }, [score]);
-
-    return (
-        <>
-            <div className="score">
-                <h1>
-                    {score.p1} - {score.p2}
-                </h1>
-            </div>
-            <Snowfall
-                color="rgba(255, 255, 255, 0.4)"
-                style={{
-                    width: "100vw",
-                    height: "100vh",
-                }}
-            />
-            <LoadingWrapper>
-                <Canvas>
-                    <Scene
-                        score={score}
-                        setScore={setScore}
-                        lastPuckReset={lastPuckReset}
-                        setLastPuckReset={setLastPuckReset}
-                    />
-                </Canvas>
-            </LoadingWrapper>
-        </>
-    );
+enum GamemodeState {
+  Minutes,
+  Points,
 }
+
+export default function App() {
+  const [score, setScore] = useState({ p1: 0, p2: 0 });
+  const [lastPuckReset, setLastPuckReset] = useState(Date.now());
+  const [currentGamemodeState, setCurrentGamemodeState] =
+    useState<GamemodeState | null>(null);
+
+  useEffect(() => {
+    console.log(score);
+    setLastPuckReset(Date.now());
+  }, [score]);
+
+  return currentGamemodeState === null ? (
+    <>
+      <GamemodeButton
+        label={"Minutes"}
+        targetState={GamemodeState.Minutes}
+        setCurrentGamemodeState={setCurrentGamemodeState}
+      />
+
+      <GamemodeButton
+        label={"Points"}
+        targetState={GamemodeState.Points}
+        setCurrentGamemodeState={setCurrentGamemodeState}
+      />
+    </>
+  ) : (
+    <>
+      <div className="score">
+        <h1>
+          {score.p1} - {score.p2}
+        </h1>
+      </div>
+      <Snowfall
+        color="rgba(255, 255, 255, 0.4)"
+        style={{
+          width: "100vw",
+          height: "100vh",
+        }}
+      />
+      <LoadingWrapper>
+        <Canvas>
+          <Scene
+            score={score}
+            setScore={setScore}
+            lastPuckReset={lastPuckReset}
+            setLastPuckReset={setLastPuckReset}
+          />
+        </Canvas>
+      </LoadingWrapper>
+    </>
+  );
+}
+
+const GamemodeButton = ({
+  label,
+  targetState,
+  setCurrentGamemodeState,
+}: {
+  label: string;
+  targetState: GamemodeState;
+  setCurrentGamemodeState: React.Dispatch<
+    React.SetStateAction<GamemodeState | null>
+  >;
+}) => {
+  return (
+    <button onClick={() => setCurrentGamemodeState(targetState)}>
+      {label}
+    </button>
+  );
+};
