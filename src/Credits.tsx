eace4fbc16a8a2c9f65b0c088af9db6d6c869aac
NoahMelle <noahmkamphuisen@gmail.com>
2025-01-17 09:27:29 +0100

    Fixed menu logic

--- src/Credits.tsx
+++ src/Credits.tsx
@@ -1,51 +1,60 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { MenuState } from "./MainMenu";
 
-export default function EmptyScene({
-  setScene,
+export default function Credits({
+    setCurrentMenuState,
 }: {
-  setScene: (scene: number) => void;
+    setCurrentMenuState: React.Dispatch<React.SetStateAction<MenuState>>;
 }) {
-  const [credits, setCredits] = useState<any[]>([]);
+    const [credits, setCredits] = useState<any[]>([]);
 
-  useEffect(() => {
-    fetch("/credits.json")
-      .then((response) => response.json())
-      .then((data) => setCredits(data))
-      .catch((error) => console.error("Error fetching credits:", error));
-  }, []);
+    useEffect(() => {
+        fetch("/credits.json")
+            .then((response) => response.json())
+            .then((data) => setCredits(data))
+            .catch((error) => console.error("Error fetching credits:", error));
+    }, []);
 
-  useEffect(() => {
-    console.log(credits);
-  }, [credits]);
+    useEffect(() => {
+        console.log(credits);
+    }, [credits]);
 
-  return (
-    <>
-      <div className="credits">
-        <ul>
-          {credits.map((credit, index) => (
-            <li key={index}>
-              <p className="credit-label">{credit.name}</p>
-              <div>
-                {credit.contributors.map(
-                  (contributor: any, contributorIndex: number) => (
-                    <p
-                      className="contributor-label"
-                      key={index + " - " + contributorIndex}
-                    >
-                      <a href={contributor.link} target="_blank">
-                        {contributor.name}
-                      </a>
-                    </p>
-                  )
-                )}
-              </div>
-            </li>
-          ))}
-        </ul>
-        <button onClick={() => setScene(0)}>
-          <p>Back</p>
-        </button>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="credits">
+                <ul>
+                    {credits.map((credit, index) => (
+                        <li key={index}>
+                            <p className="credit-label">{credit.name}</p>
+                            <div>
+                                {credit.contributors.map(
+                                    (
+                                        contributor: any,
+                                        contributorIndex: number
+                                    ) => (
+                                        <p
+                                            className="contributor-label"
+                                            key={
+                                                index + " - " + contributorIndex
+                                            }
+                                        >
+                                            <a
+                                                href={contributor.link}
+                                                target="_blank"
+                                            >
+                                                {contributor.name}
+                                            </a>
+                                        </p>
+                                    )
+                                )}
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <button onClick={() => setCurrentMenuState(MenuState.Menu)}>
+                    <p>Back</p>
+                </button>
+            </div>
+        </>
+    );
 }
